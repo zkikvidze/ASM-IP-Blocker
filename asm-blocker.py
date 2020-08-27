779e8161907d198cf8f317ccc85b93f0c68924b5
Zviad Kikvidze <1021281+zkikvidze@users.noreply.github.com>
2020-08-28 00:28:03 +0400

    Update asm-blocker.py

--- asm-blocker.py
+++ asm-blocker.py
@@ -1,5 +1,4 @@
 import requests
-import json
 import argparse
 
 
@@ -10,24 +9,22 @@ asmaddress = "CHANGEME"
 auth = "CHANGEME"
 
 
-parser = argparse.ArgumentParser(description='BIG-IP ASM IP Address Blocker')
+parser = argparse.ArgumentParser(description='F5 ASM IP Address Blocker')
 
 parser.add_argument('-a', '--address', help='IP Address', required=True)
 parser.add_argument('-p', '--policy', help='Policy Full path. Example: /Common/website.ge_policy', required=True)
+parser.add_argument('-o', '--operation', choices=['block', 'unblock'], help='Type operation. Allowed values are: block OR unblock', required=True)
 args = parser.parse_args()
 
 asmauth = "Basic " + auth
 
 
 def getPolicyId(fullPath):
-	asmurl = "https://" + asmaddress + "/mgmt/tm/asm/policies?$select=id,fullPath"
+	asmurl = "https://" + asmaddress + "/mgmt/tm/asm/policies?$select=id&$filter=name eq '" + fullPath + "'"
 	headers = {"Authorization": asmauth}
 	r = requests.get(asmurl, verify=False, headers=headers)
 	js = r.json()
-
-	for item in js['items']:
-		if item['fullPath'] == fullPath:
-			return item['id']
+	return js['items'][0]['id']
 
 
 def blockIp(ip, id):
@@ -35,6 +32,12 @@ def blockIp(ip, id):
 	payload = {"blockRequests": "always", "description": "Blocked By API Client", "ipAddress": ip, "ipMask": "255.255.255.255", "neverLearnRequests": "true"}
 	headers = {"Content-Type": "application/json", "Authorization": asmauth}
 	requests.post(asmurl, verify=False, headers=headers, json=payload)
+
+
+def unBlockIp(ip,id):
+	asmurl = "https://" + asmaddress + "/mgmt/tm/asm/policies/" + id + "/whitelist-ips?$filter=ipAddress eq '" + ip + "'"
+	headers = {"Authorization": asmauth}
+	requests.delete(asmurl, verify=False, headers=headers)
 
 
 def applyPolicy(id):
@@ -46,7 +49,9 @@ def applyPolicy(id):
 
 
 
-
-blockIp(args.address, getPolicyId(args.policy))
-applyPolicy(getPolicyId(args.policy))
-
+if args.operation == "block":
+	blockIp(args.address, getPolicyId(args.policy))
+	applyPolicy(getPolicyId(args.policy))
+if args.operation == "unblock":
+	unBlockIp(args.address, getPolicyId(args.policy))
+	applyPolicy(getPolicyId(args.policy))
